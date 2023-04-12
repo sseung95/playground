e640dd63adaada90e368571cdee0fde452e5d9e0
sseung95 <seung3657@gmail.com>
2023-04-12 23:33:16 +0900

    feat: add drag and drop image upload feature

--- src/components/common/ImageUploader/ImageUploader.tsx
+++ src/components/common/ImageUploader/ImageUploader.tsx
@@ -1,32 +1,107 @@
 import styled from '@emotion/styled';
-import React, { useRef, useState } from 'react';
+import React, {
+  ChangeEvent,
+  useCallback,
+  useEffect,
+  useRef,
+  useState,
+} from 'react';
 import { color } from '@/styles/color';
 import CloudUploadIcon from '@mui/icons-material/CloudUpload';
 import CloseIcon from '@mui/icons-material/Close';
 import Image from 'next/image';
+import { typography } from '@/styles/typography';
+
+interface ImageTypes {
+  name: string;
+  url: string;
+}
 
 const ImageUploader = () => {
   const imgInputRef = useRef<HTMLInputElement | null>(null);
-  const [images, setImages] = useState<string[]>([]);
+  const dragRef = useRef<HTMLDivElement | null>(null);
+  const [images, setImages] = useState<ImageTypes[]>([]);
+  const [isDragging, setIsDragging] = useState(false);
 
-  const handleChangeImage = (e: React.ChangeEvent) => {
+  const handleChangeImage = (e: ChangeEvent) => {
     const targetFiles = (e.target as HTMLInputElement).files as FileList;
-    const targetFilesArray = Array.from(targetFiles);
-    const selectedFiles: string[] = targetFilesArray.map((file) => {
-      return URL.createObjectURL(file);
-    });
+    handleSetImageList(targetFiles);
+  };
+
+  const handleSetImageList = (fileList: FileList) => {
+    const targetFilesArray = Array.from(fileList);
+    const selectedFiles: ImageTypes[] = targetFilesArray.map((file) => ({
+      name: file.name,
+      url: URL.createObjectURL(file),
+    }));
     setImages((prev) => [...prev, ...selectedFiles]);
   };
+
+  const handleDragIn = useCallback((e: DragEvent) => {
+    e.preventDefault();
+    e.stopPropagation();
+  }, []);
+
+  const handleDragOut = useCallback((e: DragEvent) => {
+    e.preventDefault();
+    e.stopPropagation();
+    setIsDragging(false);
+  }, []);
+
+  const handleDragOver = useCallback((e: DragEvent) => {
+    e.preventDefault();
+    e.stopPropagation();
+    setIsDragging(true);
+  }, []);
+
+  const handleDragDrop = useCallback((e: DragEvent) => {
+    e.preventDefault();
+    e.stopPropagation();
+
+    if (!e.dataTransfer) return;
+
+    const targetFiles = e.dataTransfer?.files;
+    handleSetImageList(targetFiles);
+    setIsDragging(false);
+  }, []);
+
+  const initDragEvents = useCallback(() => {
+    if (!dragRef.current) return;
+    dragRef.current.addEventListener('dragenter', handleDragIn); // dragenter: 마우스가 대상 객체의 위로 처음 진입할 때 발생
+    dragRef.current.addEventListener('dragleave', handleDragOut); // dragleave: 드래그가 끝나서 마우스가 대상 객체의 위에서 벗어날 때 발생
+    dragRef.current.addEventListener('dragover', handleDragOver); // dragover: 드래그하면서 마우스가 대상 객체의 위에 자리 잡고 있을 때 발생
+    dragRef.current.addEventListener('drop', handleDragDrop); // drop: 드래그가 끝나서 드래그하던 객체를 놓는 장소에 위치한 객체에서 발생
+  }, [handleDragDrop, handleDragIn, handleDragOut, handleDragOver]);
+
+  const removeDragEvents = useCallback(() => {
+    if (!dragRef.current) return;
+    dragRef.current.removeEventListener('dragenter', handleDragIn);
+    dragRef.current.removeEventListener('dragleave', handleDragOut);
+    dragRef.current.removeEventListener('dragover', handleDragOver);
+    dragRef.current.removeEventListener('drop', handleDragDrop);
+  }, [handleDragIn, handleDragOut, handleDragOver, handleDragDrop]);
+
+  useEffect(() => {
+    initDragEvents();
+
+    return () => removeDragEvents();
+  }, [initDragEvents, removeDragEvents]);
 
   return (
     <ImageUploaderContainer>
       <UploadContainer
+        ref={dragRef}
+        isDragging={isDragging}
         onClick={() => {
           if (!imgInputRef.current) return;
           imgInputRef.current.click();
         }}
       >
-        <UploadIcon />
+        <div>
+          <UploadIcon />
+          <p>jpg, jpeg, png</p>
+        </div>
+
         <input
           ref={imgInputRef}
           type="file"
@@ -38,18 +113,26 @@ const ImageUploader = () => {
       <ImagePreviewContainer>
         {images &&
           images.map((image, i) => (
-            <ImagePreview key={`image-uploader-${i}`}>
-              <Image src={image} alt={`이미지 ${i}`} width={120} height={80} />
-              <ImageRemoveIcon
-                onClick={() => {
-                  setImages((prevImages) =>
-                    prevImages.filter(
-                      (prevImage, prevImageIndex) => prevImageIndex !== i
-                    )
-                  );
-                }}
-              />
-            </ImagePreview>
+            <figure key={`image-uploader-${i}`}>
+              <ImagePreview>
+                <Image
+                  src={image.url}
+                  alt={`이미지 ${i}`}
+                  width={120}
+                  height={80}
+                />
+                <ImageRemoveIcon
+                  onClick={() => {
+                    setImages((prevImages) =>
+                      prevImages.filter(
+                        (prevImage, prevImageIndex) => prevImageIndex !== i
+                      )
+                    );
+                  }}
+                />
+              </ImagePreview>
+              <ImageName>{image.name}</ImageName>
+            </figure>
           ))}
       </ImagePreviewContainer>
     </ImageUploaderContainer>
@@ -64,17 +147,36 @@ const ImageUploaderContainer = styled.div`
   gap: 1.6rem;
 `;
 
-const UploadContainer = styled.div`
+const UploadContainer = styled.div<{ isDragging: boolean }>`
   display: flex;
   justify-content: center;
   align-items: center;
   height: 12rem;
-  border: 2px dashed ${color.border.default};
+  border: 2px dashed
+    ${({ isDragging }) => (isDragging ? color.primary : color.border.default)};
   border-radius: 0.8rem;
   cursor: pointer;
+  background-color: ${({ isDragging }) =>
+    isDragging ? color.secondary : '#fff'};
+
+  transition: all 0.2s;
+
+  > div {
+    text-align: center;
+
+    p {
+      ${typography.caption}
+      color: ${color.text.gray}
+    }
+  }
 
   input {
     display: none;
+  }
+
+  svg {
+    fill: ${({ isDragging }) =>
+      isDragging ? color.primary : color.border.default};
   }
 `;
 
@@ -106,10 +208,26 @@ const ImagePreview = styled.div`
 const ImageRemoveIcon = styled(CloseIcon)`
   width: 2rem;
   height: 2rem;
+  padding: 4px;
+  box-sizing: content-box;
   cursor: pointer;
   fill: ${color.text.gray};
+  border-radius: 100px;
 
   position: absolute;
   top: 0.4rem;
   right: 0.4rem;
+
+  transition: background-color 0.2s;
+
+  :hover {
+    background-color: rgba(173, 181, 189, 0.2);
+  }
+`;
+
+const ImageName = styled.figcaption`
+  max-width: 12rem;
+  padding: 0 0.4rem;
+  margin-top: 0.4rem;
+  ${typography.caption}
 `;
